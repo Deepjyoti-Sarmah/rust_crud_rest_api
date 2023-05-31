9250cc8ea72cb55acd7c44d4d718e266572d9346
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-05-31 22:04:04 +0530

    listening and serving port at 8080 , extracting id and user fron user

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct User {
     email: String,
 }
 
-// Database url
+// Database_url
 const DB_URL: &str = !env("DATABASE_URL");
 
 // constants
@@ -23,4 +23,57 @@ const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n
 const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
 
+//main function
 
+fn main() {
+    //Set database
+    if let Err(e) = set_database() {
+        println!("Error: {}", e);
+        return;
+    }
+
+    //start server and print port
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
+    println!("Server started at port 8080");
+
+    //handle the client
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_clinet(stream);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+}
+
+//handel client function
+
+
+//set database function
+fn set_database() -> Result<(), PostgresError> {
+    //Connection to database
+    let mut client = Client::connect(DB_URL, NoTls)?;
+
+    //Create table
+    client.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL
+        )",
+        &[]
+    )?;
+}
+
+//get_id function
+fn get_id(request: &str) -> &str {
+    request.split("/").nth(2).unwrap_or_default().split_whitespace().next().unwrap_or_default()
+}
+
+//deserialize user from request body with the id
+fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
+    serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
+}
